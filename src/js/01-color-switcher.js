04c77bded8ae9d3563c46cee22d7171081c2db94
olekskhom <oleks.khom@gmail.com>
2023-08-06 22:31:28 +0300

    task-1_first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
 console.log('test')
+
+// Створення змінної для інтервалу:
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+// Створення змінної для збереження ідентифікатора інтервалу:
+let intervalId = null;
+
+// Генерування випадкового кольору:
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+// Створення функцій для роботи з зміною кольору фону:
+function startChangingColor() {
+    intervalId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+    }, 1000);
+
+    startBtn.disabled = true;
+}
+
+function stopChangingColor() {
+// Очистити інтервал:
+    clearInterval(intervalId);
+
+     // Розблокувати кнопку "Start" після зупинки зміни фону:
+    startBtn.disabled = false;
+}
